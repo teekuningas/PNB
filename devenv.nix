5a6957ea87e21570ad96cf2eaf0f1d1e80b32d72
teekuningas <erkkahe@gmail.com>
2025-12-10 08:20:17 +0000

    Update

--- devenv.nix
+++ devenv.nix
@@ -34,6 +34,7 @@
   devcontainer.settings.updateContentCommand = "devenv print-dev-env >> /etc/bash.bashrc";
   devcontainer.settings.containerEnv = {
     COPILOT_GITHUB_TOKEN = "$" + "{localEnv:COPILOT_GITHUB_TOKEN}";
+    GEMINI_API_KEY = "$" + "{localEnv:GEMINI_API_KEY}";
     GIT_AUTHOR_NAME = "$" + "{localEnv:GIT_AUTHOR_NAME}";
     GIT_AUTHOR_EMAIL = "$" + "{localEnv:GIT_AUTHOR_EMAIL}";
   };
