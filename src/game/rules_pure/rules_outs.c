bd70d89fb15948d1e28a2fa3437ab248406bfb54
teekuningas <erkkahe@gmail.com>
2025-12-17 17:35:13 +0000

    Implement dummy is_runner_forced_out in rules_outs.c

---
+++ src/game/rules_pure/rules_outs.c
@@ -0,0 +1,5 @@
+#include "rules_outs.h"
+
+int is_runner_forced_out(int player_base, int player_is_on_base_flag, int ball_at_target_base_index, int taking_free_walk, int is_out_of_bounds) {
+    return 0;
+}
