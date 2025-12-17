cf703c3c6cb812c86a18195811590a3106ebc305
teekuningas <erkkahe@gmail.com>
2025-12-17 17:54:08 +0000

    Implement dummy calculate_runs in rules_runs.c

--- src/game/rules_pure/rules_runs.c
+++ src/game/rules_pure/rules_runs.c
@@ -1,3 +1,11 @@
 #include "rules_runs.h"
 
-// Implementation will go here
+int calculate_runs(int player_base, int player_original_base, int player_is_wounded, int can_make_run_of_honor, int has_made_run_on_third_base) {
+    (void)player_base;
+    (void)player_original_base;
+    (void)player_is_wounded;
+    (void)can_make_run_of_honor;
+    (void)has_made_run_on_third_base;
+    
+    return 0;
+}
