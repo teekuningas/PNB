77ce92e4ec3ab765b324c95099e7321604c69aaf
teekuningas <erkkahe@gmail.com>
2025-12-17 17:50:46 +0000

    Update tests/test_runner.c to include and run rules_runs tests

--- tests/test_runner.c
+++ tests/test_runner.c
@@ -6,6 +6,7 @@
 #include "test_catching_ai_strategy.h"
 #include "test_pitching_ai_strategy.h"
 #include "test_rules_outs.h"
+#include "test_rules_runs.h"
 #include "fixtures.h"
 #include "menu_types.h"
 #include "globals.h"
@@ -141,6 +142,9 @@ int main(int argc, char* argv[]) {
 
 	// Rules Outs tests
 	run_rules_outs_tests();
+
+	// Rules Runs tests
+	run_rules_runs_tests();
 	
 	printf("\n========================================\n");
 	printf("Tests run: %d\n", tests_run);
